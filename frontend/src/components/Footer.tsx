28b88ccffbded7b42eb5c38da97c8730efdad1d4
Yuvraj-choudhary <79357332+Yuvraj-choudhary@users.noreply.github.com>
2022-03-27 13:35:21 +0530

    main

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,19 +1,14 @@
-import {
-  FormControl,
-  Box,
-  useColorMode
-} from "@chakra-ui/react";
+import { Box, FormControl, useColorMode } from "@chakra-ui/react";
 import { IconButton } from "@material-ui/core";
 import { Close } from "@material-ui/icons";
-import AudioPlayer from "react-h5-audio-player";
-import "./styles.css";
-import ModalImage from "react-image-modal";
 import { EmojiPicker } from "react-emoji-search";
+import AudioPlayer from "react-h5-audio-player";
+import ModalImage from "react-image-modal";
+import AttachFile from "./footerIcons/AttachFile";
+import SendOrMic from "./footerIcons/SendOrMic";
+import ShowEmoji from "./footerIcons/ShowEmoji";
 import InputRoot from "./InputRoot";
-import ShowEmoji from './footerIcons/ShowEmoji';
-import AttachFile from './footerIcons/AttachFile';
-import SendOrMic from './footerIcons/SendOrMic';
-import { useState } from 'react';
+import "./styles.css";
 
 const Footer = ({
   sendMessage,
@@ -42,116 +37,130 @@ const Footer = ({
   video,
   setVideo,
   onEmojiClick,
-  fileLoading
+  fileLoading,
 }) => {
   const { colorMode } = useColorMode();
   return (
     <>
-      <>
-        {pic && (
-          <>
-            <IconButton
-              onClick={() => {
-                setPic();
-              }}
-              style={{
-            marginTop: 8,
-                color: colorMode === "dark" ? "#898787" : "#707070"
-              }}
-            >
-              <Close />
-            </IconButton>
-            <ModalImage
-              showRotation={true}
-              showZoom={true}
-              showDownload={true}
-              src={pic}
-              alt=""
-              className="image2"
+      <Box overflow="scroll">
+        <>
+          {pic && !showPicker ? (
+            <Box alignItems="center" d="flex" flexDir="column">
+              <IconButton
+                onClick={() => {
+                  setPic();
+                }}
+                style={{
+                  marginTop: 8,
+                  color: colorMode === "dark" ? "#898787" : "#707070",
+                }}
+              >
+                <Close />
+              </IconButton>
+              <ModalImage
+                showRotation={true}
+                showZoom={true}
+                showDownload={true}
+                src={pic}
+                alt=""
+                className="image2"
+              />
+            </Box>
+          ) : (
+            <></>
+          )}
+          {audio && !showPicker ? (
+            <Box alignItems="center" d="flex" flexDir="column">
+              <IconButton
+                onClick={() => {
+                  setAudio();
+                }}
+                style={{
+                  marginTop: 8,
+                  color: colorMode === "dark" ? "#898787" : "#707070",
+                }}
+              >
+                <Close />
+              </IconButton>
+              <AudioPlayer src={audio} />
+            </Box>
+          ) : (
+            <></>
+          )}
+          {gif && !showPicker ? (
+            <Box alignItems="center" d="flex" flexDir="column">
+              <IconButton
+                onClick={() => {
+                  setGif("");
+                }}
+                style={{
+                  marginTop: 8,
+                  color: colorMode === "dark" ? "#898787" : "#707070",
+                  alignSelf: "center",
+                }}
+              >
+                <Close />
+              </IconButton>
+              <ModalImage
+                showRotation={true}
+                showZoom={true}
+                src={gif}
+                alt=""
+                className="image2"
+              />
+            </Box>
+          ) : (
+            <></>
+          )}
+          {video && !showPicker ? (
+            <Box alignItems="center" d="flex" flexDir="column">
+              <IconButton
+                onClick={() => setVideo()}
+                style={{
+                  marginTop: 8,
+                  color: colorMode === "dark" ? "#898787" : "#707070",
+                  alignSelf: "center",
+                }}
+              >
+                <Close />
+              </IconButton>
+              <video src={video} controls={true} className="video"></video>
+            </Box>
+          ) : (
+            <></>
+          )}
+          {file && !showPicker ? (
+            <Box alignItems="center" d="flex" flexDir="column">
+              <IconButton
+                onClick={() => setFile()}
+                style={{
+                  marginTop: 8,
+                  color: colorMode === "dark" ? "#898787" : "#707070",
+                }}
+              >
+                <Close />
+              </IconButton>
+              <iframe src={file} className="file"></iframe>
+            </Box>
+          ) : (
+            <></>
+          )}
+        </>
+        {showPicker && !showGifPicker ? (
+          <Box h="80%" w="100%">
+            <EmojiPicker
+              emojiVersion={14.0}
+              emojiSize={45}
+              onEmojiClick={(e) => onEmojiClick(e)}
+              tabsVariant="fullWidth"
+              set="native"
+              mode={colorMode === "dark" ? "dark" : "light"}
             />
-          </>
+          </Box>
+        ) : (
+          <></>
         )}
-        {audio && (
-          <>
-            <IconButton
-              onClick={() => {
-                setAudio();
-              }}
-              style={{
-                marginTop: 8,
-                color: colorMode === "dark" ? "#898787" : "#707070"
-              }}
-            >
-              <Close />
-            </IconButton>
-            <AudioPlayer src={audio} />
-          </>
-        )}
-        {gif && (
-          <>
-            <IconButton
-              onClick={() => {
-                setGif("");
-              }}
-              style={{
-                marginTop: 8,
-                color: colorMode === "dark" ? "#898787" : "#707070"
-              }}
-            >
-              <Close />
-            </IconButton>
-            <ModalImage
-              showRotation={true}
-              showZoom={true}
-              src={gif}
-              alt=""
-              className="image2"
-            />
-          </>
-        )}
-        {video && (
-          <>
-            <IconButton
-              onClick={() => setVideo()}
-              style={{
-                marginTop: 8,
-                color: colorMode === "dark" ? "#898787" : "#707070"
-              }}
-            >
-              <Close />
-            </IconButton>
-            <video src={video} controls={true} className="video"></video>
-          </>
-        )}
-        {file && (
-          <>
-            <IconButton
-              onClick={() => setFile()}
-              style={{
-                marginTop: 8,
-                color: colorMode === "dark" ? "#898787" : "#707070"
-              }}
-            >
-              <Close />
-            </IconButton>
-            <embed src={file} className="file"></embed>
-          </>
-        )}
-      </>
-      {showPicker && !showGifPicker ? (
-        <Box h="80%" w="100%">
-          <EmojiPicker
-            emojiVersion={14.0}
-            emojiSize={45}
-            onEmojiClick={(e) => onEmojiClick(e)}
-            tabsVariant="fullWidth"
-            set="native"
-            mode={colorMode === "dark" ? "dark" : "light"}
-          />
-        </Box>
-      ) : (
-        <></>
-      )}
+      </Box>
       <FormControl
         onKeyDown={sendMessage}
         isRequired
@@ -164,38 +173,36 @@ const Footer = ({
         pt={3}
         px={2}
       >
-        <>
-          <ShowEmoji
-            showGifPicker={showGifPicker}
-            colorMode={colorMode}
-            setShowGifPicker={setShowGifPicker}
-            setShowPicker={setShowPicker}
-          />
-          <AttachFile
-            showPicker={showPicker}
-            showGifPicker={showGifPicker}
-            colorMode={colorMode}
-            setShowGifPicker={setShowGifPicker}
-            postDetails={postDetails}
-            postVideo={postVideo}
-            postFile={postFile}
-          />
-          <InputRoot newMessage={newMessage} typingHandler={typingHandler} />
-          <SendOrMic
-            newMessage={newMessage}
-            pic={pic}
-            audio={audio}
-            gif={gif}
-            video={video}
-            colorMode={colorMode}
-            sendMessageButton={sendMessageButton}
-            picLoading={picLoading}
-            audioLoading={audioLoading}
-            videoLoading={videoLoading}
-            fileLoading={fileLoading}
-            postAudio={postAudio}
-          />
-        </>
+        <ShowEmoji
+          showGifPicker={showGifPicker}
+          colorMode={colorMode}
+          setShowGifPicker={setShowGifPicker}
+          setShowPicker={setShowPicker}
+        />
+        <AttachFile
+          showPicker={showPicker}
+          showGifPicker={showGifPicker}
+          colorMode={colorMode}
+          setShowGifPicker={setShowGifPicker}
+          postDetails={postDetails}
+          postVideo={postVideo}
+          postFile={postFile}
+        />
+        <InputRoot newMessage={newMessage} typingHandler={typingHandler} />
+        <SendOrMic
+          newMessage={newMessage}
+          pic={pic}
+          audio={audio}
+          gif={gif}
+          video={video}
+          colorMode={colorMode}
+          sendMessageButton={sendMessageButton}
+          picLoading={picLoading}
+          audioLoading={audioLoading}
+          videoLoading={videoLoading}
+          fileLoading={fileLoading}
+          postAudio={postAudio}
+        />
       </FormControl>
     </>
   );
