d1200a010a12de425679e96b7c0effdcd9b88228
Yuvraj-choudhary <79357332+Yuvraj-choudhary@users.noreply.github.com>
2022-06-03 07:16:55 +0530

    main

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-const {notFound, errorHandler} = require("./middleware/errorMiddleware");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 const Pusher = require("pusher");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const pusher = new Pusher({
     appId: "1364139",
@@ -75,9 +77,24 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 
-app.get("/", (req, res) => {
-    res.send("API is running..");
-});
+// ------------------------->DeployMent<-------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running..");
+    });
+}
+
+// ------------------------->DeployMent<-------------------------
 
 app.use(notFound);
 app.use(errorHandler);
