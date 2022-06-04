9a57765d50dbf9efe5fb8a2b2f29e6b099a4dc14
Yuvraj-choudhary <79357332+Yuvraj-choudhary@users.noreply.github.com>
2022-06-04 21:20:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -76,17 +76,11 @@ app.use("/api", apiRoutes);
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname1, "/frontend/dist")));
 
     app.get("*", (req, res) =>
         res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
     );
-} else {
-    app.get("/", (req, res) => {
-        res.send("API is running..");
-    });
-}
 
 // ------------------------->DeployMent<-------------------------
 
